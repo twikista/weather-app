80af075a4855ed84d7024a9f8ab2ff7004d8aed2
viro <twikista@gmail.com>
2022-06-29 20:36:48 +0100

    handle redirect

--- src/components/main-components/renderFavorites.js
+++ src/components/main-components/renderFavorites.js
@@ -3,8 +3,30 @@ import weatherCardTop from "../weatherCard-component/weatherCardTop";
 import favorites from "../../data/favorites-data";
 
 function renderFavorite() {
-  const main = document.querySelector("main");
+  const renderNoFavorite = () => {
+    const sadFaceIcon = helper.createElement(
+      "span",
+      ["sentiment_dissatisfied"],
+      {
+        class:
+          "material-icons-outlined text-slate-800 text-[200px] block  font-light",
+      }
+    );
+
+    const text = helper.createElement("p", ["no items in favorites"], {
+      class: "uppercase text-2xl",
+    });
+
+    const wrapper = helper.createElement("div", [sadFaceIcon, text], {
+      class: "flex flex-col justify-center items-center gap-y-[50px]",
+    });
+    // const fragment = new DocumentFragment();
+    // fragment.append(sadFaceIcon, text);
+    return wrapper;
+  };
+
   const favoritesArray = favorites.favoritesData();
+  const isFavoriteEmpty = favoritesArray.length === 0;
   let fragment = new DocumentFragment();
   favoritesArray.forEach((favorite) => {
     // const article = helper.createElement(
@@ -15,10 +37,18 @@ function renderFavorite() {
     const weathercard = weatherCardTop(favorite);
     fragment.append(weathercard);
   });
-  const section = helper.createElement("section", [fragment], {
-    class: "flex flex-col",
-  });
+
+  //condtionally render section based length of favoritesArray
+  console.log(favoritesArray.length === 0);
+  const section = helper.createElement(
+    "section",
+    [isFavoriteEmpty ? renderNoFavorite() : fragment],
+    {
+      class: "flex flex-col ",
+    }
+  );
   // console.log(fragment);
+  const main = document.querySelector("main");
   main.innerHTML = "";
   main.append(section);
 }
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -3,9 +3,9 @@ import renderFavorite from "../components/main-components/renderFavorites";
 import renderSettings from "../components/main-components/settings";
 
 const routes = {
-  "/weather-app": renderHome,
+  "/weather-app/": renderHome,
   "/weather-app/favorites": renderFavorite,
-  "/weather-app/settings": renderSettings,
+  "/weather/settings": renderSettings,
 };
 
 export default routes;
